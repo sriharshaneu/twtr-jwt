9408365ce12050735f9400568ba484a61ebdb2c3
Sri Harsha Peri <peri.s@northeastern.edu>
2022-11-13 12:38:07 -0500

    changed db url string to local config

--- be/twtr.py
+++ be/twtr.py
@@ -31,8 +31,8 @@ import jwt
 g = dict()
 
 # mongo
-#mongo_client = MongoClient('mongodb://localhost:27017/')
-mongo_client = MongoClient("mongodb+srv://admin:admin@tweets.8ugzv.mongodb.net/tweets?retryWrites=true&w=majority")
+mongo_client = MongoClient('mongodb://localhost:27017/')
+# mongo_client = MongoClient("mongodb+srv://admin:admin@tweets.8ugzv.mongodb.net/tweets?retryWrites=true&w=majority")
 class MyMongo(object):
     def __init__(self, db_name):
         self.db_name = db_name
