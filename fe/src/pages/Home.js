b52d7c613493baebe442e5b6dece0b0610a8709a
Sri Harsha Peri <peri.s@northeastern.edu>
2022-11-15 19:04:33 -0500

    changed title

--- fe/src/pages/Home.js
+++ fe/src/pages/Home.js
@@ -2,29 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
     display: "flex",
     flexWrap: "wrap",
     justifyContent: "space-around",
     overflow: "hidden",
     backgroundColor: theme.palette.background.paper,
-    marginTop: '100px'
-  }
+    marginTop: "100px",
+  },
 });
 
 function Home(props) {
   const { classes } = props;
 
   return (
-      <div className={classes.root}>
-        <h1>Landing page</h1>
-      </div>
+    <div className={classes.root}>
+      <h1>Harsha's Landing page</h1>
+    </div>
   );
 }
 
 Home.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(Home);
